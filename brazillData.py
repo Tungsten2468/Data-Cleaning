6b9b0a56da32d94f3ce62a2e95aef1a5e307041d
Tomfoolery2468 <105111033+Tungsten2468@users.noreply.github.com>
2026-07-06 13:19:01 -0400

    fixed create datatables for each file

--- brazillData.py
+++ brazillData.py
@@ -3,17 +3,17 @@ import pandas as pan
 import os
 
 files = os.listdir("b_input_data")
-
+dataConnect = SQ.connect("b_output_data/brazilian_data_db.sqlite")
+curs = dataConnect.cursor()
 for file in files:
-    dataConnect = SQ.connect("b_databases/"+file[:-3]+'.sqlite')
-    curs = dataConnect.cursor()
-    
-    #curs.execute("CREATE TABLE ")
-
-    fileName = "olist_customers_dataset.csv"
-    fileRead = pan.read_csv("input_data/"+fileName)
-
-print(fileRead)
-
+    fileRead = pan.read_csv("b_input_data/"+file)
+    cols = ""
+    for header in fileRead.columns: #only add commas if the header is not the last one
+        if(fileRead.columns.get_loc(header) != len(fileRead.columns)-1):
+            cols = cols+header+", "
+        else:
+            cols = cols+header
+    curs.execute("CREATE TABLE "+file[:-4]+"("+cols+")")
 
 
+#fileRead = pan.read_csv("input_data/"+fileName)
