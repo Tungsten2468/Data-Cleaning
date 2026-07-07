bab067d8d3ed6ce98cb3c71666bfd789d96a435f
Tomfoolery2468 <105111033+Tungsten2468@users.noreply.github.com>
2026-07-07 09:56:25 -0400

    data set

--- brazillData.py
+++ brazillData.py
@@ -5,15 +5,20 @@ import os
 files = os.listdir("b_input_data")
 dataConnect = SQ.connect("b_output_data/brazilian_data_db.sqlite")
 curs = dataConnect.cursor()
-for file in files:
-    fileRead = pan.read_csv("b_input_data/"+file)
-    cols = ""
-    for header in fileRead.columns: #only add commas if the header is not the last one
-        if(fileRead.columns.get_loc(header) != len(fileRead.columns)-1):
-            cols = cols+header+", "
-        else:
-            cols = cols+header
-    curs.execute("CREATE TABLE "+file[:-4]+"("+cols+")")
+def importData():
+    for file in files:
+        fileRead = pan.read_csv("b_input_data/"+file)
+        cols = ""
+        for header in fileRead.columns: #only add commas if the header is not the last one
+            if(fileRead.columns.get_loc(header) != len(fileRead.columns)-1):
+                cols = cols+header+", "
+            else:
+                cols = cols+header
+        tableName = file[:-4]
+        
+        curs.execute("CREATE TABLE IF NOT EXISTS "+tableName+"("+cols+")")
+        fileRead.to_sql(tableName, dataConnect, if_exists="append", index=False)
 
+importData()
+dataConnect.close()
 
-#fileRead = pan.read_csv("input_data/"+fileName)
