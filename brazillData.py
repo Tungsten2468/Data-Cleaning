bc9ef551ad9adc66522d506c1b93a954dbd902b1
tr1ang <angelc142009@gmail.com>
2026-07-14 11:03:46 -0400

    repsh

--- brazillData.py
+++ brazillData.py
@@ -107,7 +107,7 @@ queryOrderEachMonth = '''SELECT
                         '''
 
 newtable = ''' SELECT 
-    i.customer_state,p.payment_type, p.payment_installments, a.price , a.freight_value, p.payment_value, r.review_score, o.order_delivered_carrier_date, o.order_delivered_customer_date, o.order_estimated_delivery_date
+    i.customer_state,p.payment_type, p.payment_installments, a.price , a.freight_value, p.payment_value, r.review_score, o.order_purchase_timestamp, o.order_delivered_customer_date, o.order_estimated_delivery_date
     FROM olist_customers_dataset i 
     JOIN olist_orders_dataset o
         ON i.customer_id = o.customer_id
