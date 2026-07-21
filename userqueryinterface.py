3cdb22e7023008e3e3c50eb5f568fb380b6a0f78
Tomfoolery2468 <105111033+Tungsten2468@users.noreply.github.com>
2026-07-21 10:51:14 -0400

    trew

--- userqueryinterface.py
+++ userqueryinterface.py
@@ -1,5 +1,12 @@
 import _sqlite3 as SQ
 
-dataConnect = SQ.connect("syn_output_data/final_reports_db.sqlite")
+fileName = "final_reports_db"
+dataConnect = SQ.connect(f"syn_output_data/{fileName}.sqlite")
 cursor = dataConnect.cursor()
 
+keywordList = []
+
+
+print(f"You are querying {fileName}. You can query the following keywords: ")
+
+userQuery = input("Enter your query using the following format: ")
