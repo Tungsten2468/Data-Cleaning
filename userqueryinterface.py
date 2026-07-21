78b02b311b185f95378270835b52f29d9f3b316c
Tomfoolery2468 <105111033+Tungsten2468@users.noreply.github.com>
2026-07-21 11:03:30 -0400

    grtew

--- userqueryinterface.py
+++ userqueryinterface.py
@@ -4,7 +4,7 @@ fileName = "final_reports.db"
 dataConnect = SQ.connect(f"syn_output_data/{fileName}")
 cursor = dataConnect.cursor()
 
-print(f"You are querying {fileName}. You can query the following keywords: ")
+print(f"You are querying {fileName}. You can query the following keywords: \n")
 for i in cursor.execute("PRAGMA table_info(empty_synthetic_data)").fetchall():
     print((i[1]))
 
