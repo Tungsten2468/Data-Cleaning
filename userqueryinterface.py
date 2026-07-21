ec0f7476089ebdc5f6dcc6d0db4b1c6a60e3d1d8
tr1ang <angelc142009@gmail.com>
2026-07-21 11:02:40 -0400

    proto

--- userqueryinterface.py
+++ userqueryinterface.py
@@ -1,12 +1,28 @@
 import _sqlite3 as SQ
 
-fileName = "final_reports_db"
-dataConnect = SQ.connect(f"syn_output_data/{fileName}.sqlite")
+fileName = "final_reports"
+dataConnect = SQ.connect(f"syn_output_data/{fileName}.db")
 cursor = dataConnect.cursor()
 
 keywordList = []
 
 
+
 print(f"You are querying {fileName}. You can query the following keywords: ")
 
-userQuery = input("Enter your query using the following format: ")
+userQuery = 'SELECT name FROM sqlite_master WHERE type="table"'
+
+activeUser = ''
+
+while activeUser != "exit":
+    cursor.execute(userQuery)
+    tableList = cursor.fetchall()
+    for i in tableList:
+        print(i[0])
+    activeUser = input("What table would you like to query? (type 'exit' to exit) ")
+    if activeUser != "exit":
+        userQuery = f'SELECT * FROM {activeUser}'
+        cursor.execute(userQuery)
+        data = cursor.fetchall()
+        for i in data:
+            print(i)
